4eea5cc720c7d226e4676dad114da82700b4a561
ha33ann <91528684+ha33ann@users.noreply.github.com>
2022-05-28 20:27:10 +0200

    Update getConnection.java

--- sqlslut/MovieDatabase/src/movie/DatabaseConnection/getConnection.java
+++ sqlslut/MovieDatabase/src/movie/DatabaseConnection/getConnection.java
@@ -12,10 +12,12 @@ public class getConnection {
         String password="123"; //SQL password
         try {
             Class.forName("com.mysql.jdbc.Driver");  //register MySQL JDBC driver
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/movie?autoReconnect=true&useSSL=false", username, password);
+            con = DriverManager.getConnection("jdbc::mysql://localhost:3306/movie", username, password);
         } catch (Exception ae) {
             System.out.println(ae);
         }
         return con;
     }
 }
+
+//jdbc:mysql://localhost:3306/movie?autoReconnect=true&useSSL=false    USE THIS TO GET RID OF SSL WARNING
